d3e3ae845bc8ca0b1e1ec0de57709f79738d5399
Rolf Stierle <rolf.stierle@gmail.com>
2016-08-13 17:35:15 +0200

    Chemical potential for the dispersive interaction implemented but not tested.

--- src/helmholtz_energies/dispersion.f90
+++ src/helmholtz_energies/dispersion.f90
@@ -122,8 +122,8 @@ contains
     end associate
 
     ! Calculation of the integrals of the perturbation theory for hard chains
-    I1 = 0.0        ! zero initialization for summation
-    I2 = 0.0        ! zero initialization for summation
+    I1 = 0.0_dp        ! zero initialization for summation
+    I2 = 0.0_dp        ! zero initialization for summation
     do i = 0, 6
       I1 = I1 + (a(0,i) &                                                     ! combinations of equations (16) & (18) or equations (A.16) & (A.18) in [1]
          & + (m_dash - 1._dp)/m_dash*a(1,i) &
@@ -175,7 +175,7 @@ contains
     real(dp) :: m2_eps2_sig3_dash                       ! mean (m(i)^2 (eps_k(i)/T)^2 sig(i)^3)
     real(dp) :: eta                                     ! packing fraction
     real(dp) :: I1, I2                                  ! integrals for chain perturbations; equations (14), (15) in [1]
-    real(dp) :: C1                                      ! derivative of compressibilities; equations (13) in [1]
+    real(dp) :: C1, C2                                  ! derivative of compressibilities; equations (13) in [1]
 
 
     associate(p => saft_para)
@@ -256,69 +256,118 @@ contains
     ! Input variables
     type(saft_parameter)                        :: saft_para      ! parameter container type
     real(dp), dimension(:), intent(in)          :: rho            ! density
-    real(dp), intent(in)                        :: Temp          ! temperature
+    real(dp), intent(in)                        :: Temp           ! temperature
     real(dp), intent(in)                        :: x              ! molefraction
 
     ! Local variables
+    real(dp)                                    :: eta            ! packing fractions
+    real(dp), dimension(:), allocatable         :: zeta3          ! partial derivative of packing fraction w.r.t. molar fraction
+    real(dp)                                    :: I1, I2         ! integrals for chain perturbations; equations (14), (15) in [1]
+    real(dp)                                    :: C1, C2         ! derivative of compressibilities; equations (13) in [1]
+    real(dp), dimension(saft_para%N_comp,saft_para%N_comp) :: sigma_ij ! array for mean PC-SAFT diameter
+    real(dp), dimension(saft_para%N_comp,saft_para%N_comp) :: eps_k_ij ! array for binary energy interactions
     real(dp)                                    :: m_dash         ! mean segment number
-    real(dp), dimension(0:3)                    :: zeta         ! weighted densities
-    real(dp), dimension(0:3, :), allocatable    :: zetax        ! partial densities
-    real(dp), dimension(:),      allocatable    :: da_hs_dx     ! partial derivative of the reduced Helmholtz energy hard-sphere contribution w.r.t. the molar fractions
-    real(dp)                                    :: summand = 0.0  ! summand, help variable
-    real(dp), dimension(:),      allocatable    :: da_hc_dx       ! partial derivative of the reduced Helmholtz energy hard-chain contribution w.r.t. the molar fractions
+    real(dp), dimension(:), allocatable         :: I1x            ! derivative of 'I1' w.r.t. molar fraction
+    real(dp), dimension(:), allocatable         :: I2x            ! derivative of 'I2' w.r.t. molar fraction
+    real(dp), dimension(:), allocatable         :: C1x            ! derivative of 'C1' w.r.t. molar fraction
+    real(dp)                                    :: m2_eps2_sig3_dash      ! mean (m(i)^2 eps_k(i)/T sig(i)^3)
+    real(dp)                                    :: m2_eps2_sig3_dash      ! mean (m(i)^2 (eps_k(i)/T)^2 sig(i)^3)
+    real(dp), dimension(:), allocatable         :: m2_eps_sig3_dash_x     ! derivative of 'm2_eps2_sig3_dash' w.r.t. molar fraction
+    real(dp), dimension(:), allocatable         :: m2_eps2_sig3_dash_x    ! derivative of 'm2_eps2_sig3_dash' w.r.t. molar fraction
+    real(dp), dimension(:,:), allocatable       :: ax             ! derivative of 'a' w.r.t. molar fraction
+    real(dp), dimension(:,:), allocatable       :: bx             ! derivative of 'b' w.r.t. molar fraction
+    real(dp), dimension(:), allocatable         :: da_disp_dx     ! derivative of 'a_tilde_disp' w.r.t. molar fractions
 
     integer                                     :: i, k           ! iteration variables
 
 
     ! Allocation
-    allocate(da_hc_dx(saft_para%N_comp), zetax(0:3, saft_para%N_comp), da_hs_dx(saft_para%N_comp) )
+    allocate( zeta3(N_comp), I1x(saft_para%N_comp), I2x(saft_para%N_comp), &
+            & C1x(saft_para%N_comp), &
+            & m2_eps_sig3_dash_x(saft_para%N_comp), &
+            & m2_eps2_sig3_dash_x(saft_para%N_comp), &
+            & ax(0:6, saft_para%N_comp), bx(0:6, saft_para%N_comp) )
 
     ! Mean segment number
     m_dash = sum(saft_para%m*x)                                     ! equation (A.5) in [1]
 
-    ! Calculation of the summand according to equation (A.35) in [1]
-    do i = 1, saft_para%N_comp
-      summand = summand + x(i)*(m(i)-1._dp)/g_hs_ij(saft_para, rho, x, i, i) &
-              & * dg_hs_ij_dxk(saft_para, rho, x, i, i)
+    ! Calculation of combination rules
+    do i = 1, p%N_comp
+      do j = 1, p%N_comp
+        sigma_ij(j,i) = 0.5*(p%m(j) + p%m(i))                       ! mean segment diameter; equation (23) or (A.14) in [1]
+        eps_k_ij(j,i) = sqrt(p%eps_k(j)*p%eps_k(i)) &
+                      & * (1._dp - p%k_ij(j,i))                     ! binary interaction parameter; equation (24) or (A.15) in [1]
+        m2_eps_sig3_dash = m2_eps_sig3_dash &                       ! abbreviation (A.12) in [1]
+                         & + x(j) * x(i) * p%m(j) * p%m(i) &
+                         & * eps_k_ij(j,i)/Temp * sigma_ij(j,i)**3
+        m2_eps2_sig3_dash = m2_eps_sig3_dash &                      ! abbreviation (A.13) in [1]
+                          & + x(j) * x(i) * p%m(j) * p%m(i) &
+                          & * (eps_k_ij(j,i)/Temp)**2 * sigma_ij(j,i)**3
+      end do
     end do
 
-    ! Weighted densities
-    do n = 0, 3
-      zeta(n) = PI/6._dp * rho * sum(x * saft_para%m * saft_para%d**n)    ! equation (9) or (A.8) in [1]
+    ! Calculation of packing fraction 'eta' and 'zeta3'
+    eta   = PI/6._dp * rho * sum(x * p%m * p%d**3)                  ! equation (A.20) in [1]
+    zeta3 = PI/6._dp * rho * saft_para%m * saft_para%d**3           ! equation (A.34) in [1]
+
+    ! Calculation of the integrals of the perturbation theory for hard chains
+    !  and their derivatives w.r.t eta
+    I1     = 0.0_dp     ! zero initialization for summation
+    I2     = 0.0_dp     ! zero initialization for summation
+    detaI1 = 0.0_dp     ! zero initialization for summation
+    detaI2 = 0.0_dp     ! zero initialization for summation
+    do i = 0, 6
+      I1 = I1 + (a(0,i) &                                                       ! combinations of equations (16) & (18) or equations (A.16) & (A.18) in [1]
+         & + (m_dash - 1._dp)/m_dash*a(1,i) &
+         & + (m_dash - 1._dp)/m_dash * (m_dash - 2._dp)/m_dash * a(2,i)) * eta**i
+      I2 = I2 + (b(0,i) &                                                       ! combinations of equations (17) & (19) or equations (A.17) & (A.19) in [1]
+         & + (m_dash - 1._dp)/m_dash*b(1,i) &
+         & + (m_dash - 1._dp)/m_dash * (m_dash - 2._dp)/m_dash * b(2,i)) * eta**i
+      ax(i,:) = m / m_dash * a(1,i) + m / m_dash * a(2,i)                       ! equation (A.44) in [1]
+      bx(i,:) = m / m_dash * b(1,i) + m / m_dash * b(2,i)                       ! equation (A.45) in [1]
+      I1x = I1x + (a(0,i) &                                                     ! equation (A.42) in [1]
+         & + (m_dash - 1._dp)/m_dash*a(1,i) &
+         & + (m_dash - 1._dp)/m_dash * (m_dash - 2._dp)/m_dash * a(2,i)) &
+         & * i * zeta3 * eta**(i-1) &
+         & + ax(i,:) * eta**i
+      I2x = I2x + (b(0,i) &                                                     ! equation (A.43) in [1]
+         & + (m_dash - 1._dp)/m_dash*b(1,i) &
+         & + (m_dash - 1._dp)/m_dash * (m_dash - 2._dp)/m_dash * b(2,i)) &
+         & * i * zeta3 * eta**(i-1) &
+         & + ax(i,:) * eta**i
     end do
 
-    ! Partial densities
-    do n = 0, 3
-      zetax(n,k) = PI/6._dp * rho * saft_para%m(k) * saft_para%d(k)**n    ! equation (A.34) in [1]
+    ! Calculation of C's
+    C1 = 1._dp / (1._dp + m_dash*(8._dp*eta - 2._dp*eta**2)/(1._dp - eta)**4 &  ! equation (13), (25) or (A.11); attention: equation (A.11) is missing the reziprocal (-)^-1 on the last line
+       & + (1._dp - m_dash)*(20._dp*eta - 27._dp*eta**2 + 12._dp*eta**3 &
+       & - 2._dp*eta**4)/((1._dp - eta)*(2._dp - eta))**2)
+    C2 = - C1**2 * ( m_dash*(-4._dp*eta**2 + 20._dp*eta + 8._dp)/(1._dp-eta)**5  & ! equation (A.31) in [1]
+       & (1._dp - m_dash) * (2._dp*eta**3 + 12._dp*eta**2 - 48._dp*eta + 40._dp) &
+       &  / ((1._dp-eta)*(2._dp-eta))**3 )
+    C1x = C2*zeta3 - C1**2* ( m*(8._dp*eta - 2._dp*eta**2)/(1._dp-eta)**4  &
+        & - m*(20._dp*eta-27._dp*eta**2+12._dp*eta**3-2._dp*eta**4) &
+        & / ((1._dp-eta)*(2._dp-eta))**2 )
+
+    ! Calculation of abbreviations
+    do k = 1, saft_para%N_Comp
+      m2_eps_sig3_dash_x(k)  = 2._dp*saft_para%m(k) &
+                             & * sum( x*m*(eps_k_ij(:,k)/Temp)   *sigma_ij(:,k) )
+      m2_eps2_sig3_dash_x(k) = 2._dp*saft_para%m(k) &
+                             & * sum( x*m*(eps_k_ij(:,k)/Temp)**2*sigma_ij(:,k) )
     end do
 
-    ! Partial derivative of the residual reduced Helmholtz energy 'a_hs' w.r.t.
-    !  the molar fraction 'x_k' according to equation (A.36) in [1]
+    ! Calculation of the derivative of the dispersion contribution to the
+    !  reduced Helmholtz energy w.r.t. the molar fractions
+    da_disp_dx = -2._dp*PI* (I1x*m2_eps_sig3_dash + I1*m2_eps_sig3_dash_x ) &           ! equation (A.38) in [1]
+               & - PI*rho*( (m*C1*I2+m_dash*C1x*I2+m_dash*C1*I2x) * m2_eps2_sig3_dash &
+               & + m_dash*C1*I2*m2_eps2_sig3_dash_x )
+
+    ! Chemical potential contribution of the dispersive interaction
     do k = 1, saft_para%N_comp
-      da_hs_dx(k) = -zetax(0,k)/zeta(0) * a_tilde_hs(saft_para,rho,x) &               ! equation (A.36) in [1]
-            & + 1._dp* ( (3._dp*( zetax(1,k)*zeta(2) + zeta(1)*zetax(2,k) )) &
-            & /(1._dp - zeta(3)) &
-            & + (3._dp*zeta(1)*zeta(2)*zetax(3,k))/(1._dp-zeta(3))**2 &
-            & + (3._dp*zeta(2)*zetax(2,k))/(zeta(3)*(1._dp-zeta(3))**2) &
-            & + (zeta(2)**3*zetax(3,k)*(3._dp*zeta(3)-1._dp)) &
-            & / (zeta(3)**2 * (1._dp-zeta(3))**3 ) &
-            & + ((3._dp*zeta(2)**2*zetax(2,k)*zeta(3) - 2._dp*zeta(2)**3*zetax(3,k)) &
-            & / (zeta(3)**3) - zeta(0,k)) * log(1._dp-zeta(3))   &
-            & + (zeta(0) - zeta(2)**3/zeta(3)**2) * zetax(3,k)/(1._dp-zeta(3)) )
+      beta_mu_hs(k) = a_tilde_disp(saft_para, rho, Temp, x) + (Z_disp - 1._dp) &          ! equation (A.33) in [1]
+                    & + da_disp_dx(k) - sum( x * da_disp_dx )
     end do
 
-    ! Calculation of the partial derivative of the hard-chain contribution to
-    !  the reduced Helmholtz energy 'a_hc' w.r.t. the molar fractions
-    do k = 1, saft_para%N_comp
-      da_hc_dx(k) = m(k)*a_tilde_hs(saft_para, rho, x) + m_dash*da_hs_dx(k) &   ! equation (A.35) in [1]
-                  & - summand
-    end do
-
-    ! Calculation of the hard chain contribution to the chemical potential
-    do k = 1, saft_para%N_comp
-      beta_mu_hc(k) = a_tilde_hc(saft_para, rho, x) + (Z_hc - 1._dp) + da_hc_dx(k)
-          & - sum( x * da_hc_dx )
-    end do
 
  end function beta_mu_hc
   ! ############################################################################
